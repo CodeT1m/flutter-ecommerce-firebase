d1e9e0567a1495e3560642823e4810d978de58ea
Timmy <ileladewa.adekunle@bfinesystems.com>
2023-02-16 11:37:14 +0800

    init UML diagrams

--- lib/constants/lists.dart
+++ lib/constants/lists.dart
@@ -3,3 +3,4 @@ import 'package:flutter_ecommerce_firebase/constants/consts.dart';
 const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
 
 const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+const secondSlidersList = [imgSs1, imgSs1, imgSs3, imgSs4];
--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_ecommerce_firebase/constants/consts.dart';
+import 'package:flutter_ecommerce_firebase/widgets_common/home_buttons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,25 +17,136 @@ class _HomeScreenState extends State<HomeScreen> {
       color: lightGrey,
       width: context.screenWidth,
       height: context.screenHeight,
-      child: SafeArea(child: Column(children: [
-        Container(
-          color: lightGrey,
-          alignment: Alignment.center,
-          height: 60,
-          child: TextFormField(
-            decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.search),
-              filled: true,
-              fillColor: whiteColor,
-              hintText: searchanyting,
-              hintStyle: TextStyle(color: textfieldGrey)
+      child: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            color: lightGrey,
+            alignment: Alignment.center,
+            height: 60,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.search),
+                  filled: true,
+                  fillColor: whiteColor,
+                  hintText: searchanyting,
+                  hintStyle: TextStyle(color: textfieldGrey)),
             ),
           ),
-        ),
-        VxSwiper.builder(itemCount: slidersList.length, itemBuilder: (context, index){
-          return Container();
-        })
-      ],)),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  //swipers brands
+                  VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 150,
+                      enlargeCenterPage: true,
+                      itemCount: slidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          slidersList[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      }),
+                  10.heightBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                        2,
+                        (index) => homeButtons(
+                              height: context.screenHeight * 0.15,
+                              width: context.screenWidth / 2.5,
+                              icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                              title: index == 0 ? todayDeal : flashsale,
+                            )),
+                  ),
+                  10.heightBox,
+
+                  // 2nd swiper
+                  VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 150,
+                      enlargeCenterPage: true,
+                      itemCount: secondSlidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          secondSlidersList[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      }),
+
+                  // category buttons
+                  10.heightBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                        3,
+                        (index) => homeButtons(
+                            height: context.screenHeight * 0.15,
+                            width: context.screenWidth / 3.5,
+                            icon: index == 0
+                                ? icTopCategories
+                                : index == 1
+                                    ? icBrands
+                                    : icTopSeller,
+                            title: index == 0
+                                ? topCategories
+                                : index == 1
+                                    ? brand
+                                    : topSellers)),
+                  ),
+
+                  //featured categories
+                  10.heightBox,
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .size(18)
+                          .color(darkFontGrey)
+                          .fontFamily(semibold)
+                          .make()),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .size(18)
+                          .color(darkFontGrey)
+                          .fontFamily(semibold)
+                          .make()),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .size(18)
+                          .color(darkFontGrey)
+                          .fontFamily(semibold)
+                          .make()),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .size(18)
+                          .color(darkFontGrey)
+                          .fontFamily(semibold)
+                          .make()),
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
